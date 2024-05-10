2c15a81b499a6ee215db5e0ba74fbb3edde99eff
abhi <aasusgod@gmail.com>
2024-05-10 19:46:00 +0530

    chore: Update .gitignore and sign-in page layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return <div className="">
+    <div className="flex items-center justify-center">
+<SignIn path="/sign-in" />
+    </div>
+    </div>;
 }
