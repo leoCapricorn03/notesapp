606a80f6c0735e2f525f1d2b905ed3c015206906
abhi <aasusgod@gmail.com>
2024-05-10 15:09:16 +0530

    Merge pull request #1 from capricorn03/main
    chore: Update environment variables and remove unused file

---
+++ app/create-notes/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateNotes from '@/components/forms/Createnotes'
+
+const page = () => {
+  return (
+    <div>
+        <div className="">
+            <CreateNotes/>
+        </div>
+    </div>
+  )
+}
+
+export default page
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
+
 const HomePage = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
@@ -13,9 +14,9 @@ const HomePage = () => {
             Keep track of your tasks and write down your notes easily.
           </p>
           <div className="flex justify-center">
-            <Link href="/tasks" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mr-4">
+            <Link href="/create-notes" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mr-4">
               
-                View Tasks
+                Create Notes
              
             </Link>
             <Link href="/notes" className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-full">
