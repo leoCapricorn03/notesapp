fe09504f387e12fd5423772acf4dee4b352964cb
abhi <aasusgod@gmail.com>
2024-05-10 13:48:22 +0530

    chore: Update environment variables and remove unused file

---
+++ app/create-notes/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateNotes from '@/components/forms/Createnotes'
+
+const page = () => {
+  return (
+    <div>page
+        <div className="">
+            <CreateNotes/>
+        </div>
+    </div>
+  )
+}
+
+export default page
